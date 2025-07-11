ee13eca822c2dd7b03a43614eecee9c2eaf565d3
Omolade37 <yetundesolanke2019@gmail.com>
2025-07-11 11:11:39 +0100

    background changed

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,9 @@
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+        nav = document.getElementById(navId);
 
     if (toggle && nav) {
-        toggle.addEventListener("click", ()=> {
+        toggle.addEventListener("click", () => {
             nav.classList.toggle("show");
         })
         
@@ -27,5 +27,40 @@ function linkAction() {
 }
     
 navLink.forEach(n => n.addEventListener("click", linkAction));
+
+
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+
+    
+})
+
+
+
+sr.reveal(".home_title", {})
+sr.reveal(".button", { delay: 200 })
+sr.reveal(".home_img", { delay: 400 })
+sr.reveal(".home_social-icons", { interval: 200 });
+
+
+sr.reveal(".about_img", {});
+sr.reveal(".about_subtitle", { delay: 200 })
+sr.reveal(".about_text", { delay: 400 })
+
+sr.reveal(".skills_subtitle", {})
+sr.reveal(".skills_text", { delay: 200})
+sr.reveal(".skills_data", { interval: 200})
+sr.reveal(".about_img", { delay: 400 })
+
+sr.reveal(".work_img", { interval: 200 })
+
+sr.reveal(".contact_input",{interval:200})
+
+
+
     
 
